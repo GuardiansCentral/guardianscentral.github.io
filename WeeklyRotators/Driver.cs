4140fa95cbf89edf3bd7136827c3ed857af572ce
WCartagenaASC <wesleycartagena23@gmail.com>
2024-08-31 00:35:47 -0400

    getting public milestones is no longer needed

--- WeeklyRotators/Driver.cs
+++ WeeklyRotators/Driver.cs
@@ -39,30 +39,7 @@ class Program{
         string UserId = configFile.Get<string>("UserId");
         string Password = configFile.Get<string>("Password");
 
-
-        PublicMilestonesResponse.RootObject PublicMilestonesObject = await BungieApiRequests.GetPublicMilestonesRequest(BungieRootPath, GetPublicMilestoneEndpoint, GuardiansCentralApiKey);
-        Log.Information(JsonConvert.SerializeObject(PublicMilestonesObject));
         List <long> weeklyRotatorTableHashes = new List<long>();
-        weeklyRotatorTableHashes.Add(2122313384);
-        weeklyRotatorTableHashes.Add(1042180643);
-        weeklyRotatorTableHashes.Add(910380154);
-        weeklyRotatorTableHashes.Add(3881495763);
-        weeklyRotatorTableHashes.Add(1441982566);
-        weeklyRotatorTableHashes.Add(1374392663);
-        weeklyRotatorTableHashes.Add(2381413764);
-        weeklyRotatorTableHashes.Add(107319834);
-        weeklyRotatorTableHashes.Add(2823159265);
-        weeklyRotatorTableHashes.Add(1262462921);
-        weeklyRotatorTableHashes.Add(2582501063);
-        weeklyRotatorTableHashes.Add(2032534090);
-        weeklyRotatorTableHashes.Add(1077850348);
-        weeklyRotatorTableHashes.Add(4078656646);
-        weeklyRotatorTableHashes.Add(313828469);
-        weeklyRotatorTableHashes.Add(2668737148);
-        weeklyRotatorTableHashes.Add(1221538367);
-        weeklyRotatorTableHashes.Add(509188661);
-        weeklyRotatorTableHashes.Add(196691221);
-        weeklyRotatorTableHashes.Add(3883295757);
         WeeklyRotatorsTable.BuildWeeklyRotatorsTable(PublicMilestonesObject, Server, Database, UserId, Password, weeklyRotatorTableHashes);
         //UpdateActiveWeeklyRotators.UpdateTable(PublicMilestonesObject, Server, Database, UserId, Password);
 
